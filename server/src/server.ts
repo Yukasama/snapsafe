4ee5a8f4b4c0b029ff54554dbf4e1fbe5b63faa4
Marius Müller <marius@bscnet.de>
2025-07-02 18:23:32 +0200

    fix: server listen to all interfaces

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import routes from "./routes";
-import { webcrypto } from "crypto";
-import db from "./db";
 
 const app = express();
 const PORT = 3000;
+const BASE_URL = "0.0.0.0";
 
 app.use(express.json({ limit: "10mb" }));
 
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`);
+app.listen(PORT, BASE_URL, () => {
+  console.log(`🚀 Server ready at http://${BASE_URL}:${PORT}`);
 });
