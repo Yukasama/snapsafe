7fa8cdbb40b08a1ff153e352dc272819fff417c2
Marius Müller <marius@bscnet.de>
2025-07-03 02:04:29 +0200

    fix: send timestamp

--- client/app/select-contacts/index.tsx
+++ client/app/select-contacts/index.tsx
@@ -109,6 +109,7 @@ export default function ContactSelectionScreen() {
           encryptedKey: encryptedAESKey,
           content: encryptedImage,
           type: "image",
+          timestamp: Date.now(),
         });
       }
 
